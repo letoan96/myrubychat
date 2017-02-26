37ead38a11c61ae0c1d2d992c047a5d87758d70b
letoan <letranthetoan@gmail.com>
2017-02-26 15:28:35 +0700

    almost done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
 	has_many :received_messages, class_name: 'Message', foreign_key: 'recipient_id'
 	has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
 	has_many :friends, class_name: 'Friend', foreign_key: 'friend_id'
-	has_many :users, class_name: 'Friend', foreign_key: 'user_id'
 	has_many :users_block, class_name: 'Block', foreign_key: 'user_id'
 	has_many :blocked_users_block, class_name: 'Block', foreign_key: 'blocked_user_id'
 	def to_s
